ab8002fb699d1c98389328fd9685d432bd3be549
Mikal Kotadia <mikal@ualberta.ca>
2023-01-07 13:08:14 -0700

    added pynput dependency

--- src/mappings.py
+++ src/mappings.py
@@ -1,10 +1,17 @@
 import os
 import sys
 import math
+# import pywin
+import win32api
+import win32gui
+import win32con
+from pynput.keyboard import Key, Controller
+keyboard = Controller()
 
 
 def main():
-    print("hello world")
+    active_window: int = win32gui.GetForegroundWindow()
+    win32gui.ShowWindow(active_window, win32con.SW_MINIMIZE)
 
 
 if __name__ == '__main__':

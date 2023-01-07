11e2e27772492fbb388e609798bb7750e28169a7
Mikal Kotadia <mikal@ualberta.ca>
2023-01-07 13:10:58 -0700

    WIP creating a keyboard

--- src/mappings.py
+++ src/mappings.py
@@ -6,10 +6,10 @@ import win32api
 import win32gui
 import win32con
 from pynput.keyboard import Key, Controller
-keyboard = Controller()
 
 
 def main():
+    keyboard = Controller()
     active_window: int = win32gui.GetForegroundWindow()
     win32gui.ShowWindow(active_window, win32con.SW_MINIMIZE)
 

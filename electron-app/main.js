9289b849c2fd9581aaf2704f1ddac04e4d27cec9
jgaribsin <jonathan.garibsingh@gmail.com>
2023-01-08 01:19:55 -0700

    integrate python script into ui

--- electron-app/main.js
+++ electron-app/main.js
@@ -1,13 +1,19 @@
+'use strict';
+
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const nodePath = require('path');
+const nodeChildProcess = require('child_process');
 const { PythonShell } = require('python-shell');
+const electronIpcMain = require('electron').ipcMain;
 
 const createWindow = () => {
 	const win = new BrowserWindow({
 		width: 800,
 		height: 600,
 		webPreferences: {
-			preload: path.join(__dirname, 'preload.js'),
+			nodeIntegration: false,
+			contextIsolation: true,
+			preload: nodePath.join(__dirname, 'preload.js'),
 		},
 	});
 
@@ -28,4 +34,28 @@ app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
 		app.quit();
 	}
+});
+
+electronIpcMain.on('runScript', () => {
+	// Windows
+	let script = nodeChildProcess.spawn('cmd.exe', [
+		'/c',
+		'test.bat',
+		'arg1',
+		'arg2',
+	]);
+
+	console.log('PID: ' + script.pid);
+
+	script.stdout.on('data', (data) => {
+		console.log('stdout: ' + data);
+	});
+
+	script.stderr.on('data', (err) => {
+		console.log('stderr: ' + err);
+	});
+
+	script.on('exit', (code) => {
+		console.log('Exit Code: ' + code);
+	});
 });
